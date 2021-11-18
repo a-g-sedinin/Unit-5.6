b3f065f55ef59076601ea17e7db95abd0f82c09f
LunacyWithin <s2k-r59@mail.ru>
2021-11-19 00:24:22 +0500

    Add some methods

--- Unit-5.6/Program.cs
+++ Unit-5.6/Program.cs
@@ -11,59 +11,106 @@ namespace Unit_5._6
         static void Main(string[] args)
         {
 
-
-            var UserData = GetUserData();
-
+           PrintData(GetUserData());
 
         }
 
-        public void GetUserData()
+        static (string , string , int , int , string[] ,int , string[] ) GetUserData()
         {
             Console.WriteLine("Укажите свое имя, пожалуйста.");
             string Name = Console.ReadLine();
             Console.WriteLine("Укажите свою фамилию, пожалуйста.");
             string SName = Console.ReadLine();
             Console.WriteLine("Укажите свой возраст, пожалуйста.");
-            
-            
-            int.TryParse(Console.ReadLine(), out int Age);
-
-
-
-        }
-        static bool AskPets()
-        {
-            bool Correct = false;
-            while (Correct!=true)
+            int Age = CheckValue();
+            string[] PetsNames = AskPets();
+            Console.WriteLine("Сколько у Вас любимых цветов?");
+            int ColoursCount = CheckValue();
+            string[] FavColours = Colours(ColoursCount);
+            if (PetsNames != null) return (Name, SName, Age, PetsNames.Length, PetsNames, ColoursCount, FavColours);
+            else
             {
-                Console.WriteLine("Есть ли у Вас домашние животные? \n Y - ДА, N - Нет");
-                string PetExist = Console.ReadLine();
+                PetsNames[0] = "Нет имен";
+                return (Name, SName, Age, 0, PetsNames, ColoursCount, FavColours);
 
-                if (PetExist == "Y")
-                {
-                    Console.WriteLine("Солько их?");
-
-
-
-                    Correct = true;
-                }
-                if (PetExist == "N")
-                {
-                    Correct = true;
-                }
             }
-            
-
-
-
-
-
+                
+                
 
         }
+        static void PrintData((string, string, int, int, string[], int, string[]) obj)
+        {
+            Console.WriteLine($"Вас зовут: {obj.Item1} {obj.Item2},Вам {obj.Item3}лет. У Вас {obj.Item4} питомцев. Их имена: ");
+            foreach (string PName in obj.Item5)
+            {
+                Console.WriteLine($"{PName}, ");
+            }
+            Console.WriteLine($"\n У Вас {obj.Item6} любимых цветов. Их названия: ");
+            foreach (string CName in obj.Item7)
+            {
+                Console.WriteLine($"{CName}, ");
+            }
+        }
+        
+        
+        static string[] AskPets()
+        {
+            int Count = 0;
+            
+            Console.WriteLine("Есть ли у Вас домашние животные? \n Y - ДА, N - Нет");
+            string PetExist = Console.ReadLine();
+
+            if (PetExist == "Y")
+            {
+                
+                Console.WriteLine("Сколько их у Вас?");
+                Count = CheckValue();
+                return PetsNames(Count);
+            }
+            else if (PetExist != "N")
+            {
+              AskPets();
+            }
+            return null;
+        }
+        static int CheckValue ()
+        {
+            Console.WriteLine("Укажите корректное значение, пожалуйста.");
+            string val = Console.ReadLine();
+            bool Correct = int.TryParse(val, out int value);
+            if ( (Correct != true) || (value < 1) )
+            {
+                Console.WriteLine("Указано некорректное значение.");
+                CheckValue();
+            }
+            return value;
+        }
+
         static string[] PetsNames(int Count)
         {
-
+            string[] Names = new string[Count];
+            int i = 0;
+            while (i < Count)
+            {
+                Console.WriteLine("Укажите имя питомца, пожалуйста.");
+                Names[i] = Console.ReadLine();
+                i++;
+            }
+            return Names;
         }
-
+        static string[] Colours(int Count)
+        {
+            string[] Clr = new string[Count];
+            int i = 0;
+            foreach (string Colour in Clr)
+            {
+                Console.WriteLine("Укажите любимый цвет, пожалуйста.");
+                
+                Clr[i] = Console.ReadLine();
+                i++;
+                
+            }
+            return Clr;
+        }
     }
 }
