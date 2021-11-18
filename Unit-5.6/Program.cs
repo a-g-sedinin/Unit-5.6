5ddafa21f3f8575e15c87b8dc047ebdfc38f07cf
a-g-sedinin <s2k-r59@mail.ru>
2021-11-18 17:27:52 +0500

    Added some code

--- Unit-5.6/Program.cs
+++ Unit-5.6/Program.cs
@@ -10,6 +10,60 @@ namespace Unit_5._6
     {
         static void Main(string[] args)
         {
+
+
+            var UserData = GetUserData();
+
+
         }
+
+        public void GetUserData()
+        {
+            Console.WriteLine("Укажите свое имя, пожалуйста.");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Укажите свою фамилию, пожалуйста.");
+            string SName = Console.ReadLine();
+            Console.WriteLine("Укажите свой возраст, пожалуйста.");
+            
+            
+            int.TryParse(Console.ReadLine(), out int Age);
+
+
+
+        }
+        static bool AskPets()
+        {
+            bool Correct = false;
+            while (Correct!=true)
+            {
+                Console.WriteLine("Есть ли у Вас домашние животные? \n Y - ДА, N - Нет");
+                string PetExist = Console.ReadLine();
+
+                if (PetExist == "Y")
+                {
+                    Console.WriteLine("Солько их?");
+
+
+
+                    Correct = true;
+                }
+                if (PetExist == "N")
+                {
+                    Correct = true;
+                }
+            }
+            
+
+
+
+
+
+
+        }
+        static string[] PetsNames(int Count)
+        {
+
+        }
+
     }
 }
